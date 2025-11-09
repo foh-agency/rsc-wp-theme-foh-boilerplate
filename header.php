66d69d7d02e93929acc7b06bad836adffe877d1b
Eva Larumbe <evalarumbe@gmail.com>
2025-11-09 20:01:28 +0100

    Refactor large files into small ones. Prefix JS classes (#4)

--- header.php
+++ header.php
@@ -26,41 +26,4 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foh' ); ?></a>
 
-	<header class="
-		js-masthead
-		flex
-		justify-between
-		">
-		<div class="js-header-logo">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$foh_description = get_bloginfo( 'description', 'display' );
-			if ( $foh_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $foh_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div>
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="foh-header-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'foh' ); ?></button>
-			<!-- Header menu location -->
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'header',
-					'menu_id'        => 'foh-header-menu',
-					'fallback_cb'    => false,
-				)
-			);
-			?>
-		</nav>
-	</header>
+	<?php get_template_part( 'template-parts/header/header', 'masthead' ); ?>
