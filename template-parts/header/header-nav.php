8c43b2a5da8623200f6856469820a709171319e2
Eva Larumbe <evalarumbe@gmail.com>
2025-11-16 16:25:35 +0100

    WIP, errors: copy in the menu from lab (to be adapted)

--- template-parts/header/header-nav.php
+++ template-parts/header/header-nav.php
@@ -10,13 +10,15 @@
 <nav id="site-navigation" class="main-navigation">
 	<button class="js-menu-toggle" aria-controls="foh-header-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'foh' ); ?></button>
 	<!-- Header menu location -->
-	<?php
-	wp_nav_menu(
-		array(
-			'theme_location'  => 'header',
-			'menu_id'			  	=> 'foh-header-menu',
-			'fallback_cb'		  => false,
-		)
-	);
-	?>
+  <div class="foh-drawer">
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location'  => 'header',
+        'menu_id'			  	=> 'foh-header-menu',
+        'fallback_cb'		  => false,
+      )
+    );
+    ?>
+  </div>
 </nav>
