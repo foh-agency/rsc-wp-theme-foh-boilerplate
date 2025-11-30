366ad96a2109024bede7a04193f4eb8192fd98c5
Eva Larumbe <evalarumbe@gmail.com>
2025-11-30 15:44:45 +0100

    No-JS: Create links for open and close

--- template-parts/header/header-nav.php
+++ template-parts/header/header-nav.php
@@ -8,10 +8,11 @@
 ?>
 
 <nav id="site-navigation" class="main-navigation">
-	<a class="js-menu-toggle-link"href="#main-nav-content"><?php esc_html_e( 'Menu link', 'foh' ); ?></a>
-	<button class="js-menu-toggle-button hidden" aria-controls="foh-header-menu" aria-expanded="false"><?php esc_html_e( 'Menu button', 'foh' ); ?></button>
+	<a class="js-menu-link--open md:hidden"href="#main-nav-content"><?php esc_html_e( 'Open menu', 'foh' ); ?></a>
+	<button class="js-menu-toggle-button hidden" aria-controls="foh-header-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'foh' ); ?></button>
 	<!-- Header menu location -->
   <div id="main-nav-content" class="foh-drawer">
+    <a class="js-menu-link--close md:hidden"href="#primary"><?php esc_html_e( 'Close menu', 'foh' ); ?></a>
     <?php
     wp_nav_menu(
       array(
