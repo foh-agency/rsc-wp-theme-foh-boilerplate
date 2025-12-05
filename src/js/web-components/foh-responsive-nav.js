172cdec45dd3b7e1cf6db2784b6e58a5a00415f8
Eva Larumbe <evalarumbe@gmail.com>
2025-12-06 00:54:34 +0100

    WIP: Revert nested custom element to repair styles that wouldn't render

--- src/js/web-components/foh-responsive-nav.js
+++ src/js/web-components/foh-responsive-nav.js
@@ -3,47 +3,56 @@
  * Some of this functionality was included with Underscores in navigation.js
  */
 export default class ResponsiveNav extends HTMLElement {
-  
+  static siteNavigation = document.querySelector('#site-navigation');
+
   constructor() {
     super();
     // Return early if the navigation don't exist.
-    this.siteNavigation = this.querySelector('#site-navigation'); // private property
-    if (!this.siteNavigation) {
+    if (!ResponsiveNav.siteNavigation) {
       return;
     }
     // Return early if the button doesn't exist.
-    this.toggleButton = this.siteNavigation.querySelector('.js-menu-toggle-button');
+    this.toggleButton = ResponsiveNav.siteNavigation.querySelector('.js-menu-toggle-button');
     if ('undefined' === typeof this.toggleButton) {
       return;
     }
     // Hide menu toggle button if menu is empty and return early.
-    this.menu = this.siteNavigation.getElementsByTagName('ul')[0];
+    this.menu = ResponsiveNav.siteNavigation.getElementsByTagName('ul')[0];
     if ('undefined' === typeof this.menu) {
       this.toggleButton.style.display = 'none';
       return;
     }
 
+    // bind event handlers
     this.initEventHandlers = this.initEventHandlers.bind(this);
-    this.initEventHandlers(this.siteNavigation);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClickOffMenu = this.handleClickOffMenu.bind(this);
+
+    this.initEventHandlers();
   }
 
-  initEventHandlers(siteNavigation) {
-    // Toggle the .js-toggled class and the aria-expanded value each time the button is clicked.
-    this.toggleButton.addEventListener('click', function () {
-      if (this.toggleButton.getAttribute('aria-expanded') === 'true') {
-        siteNavigation.classList.toggle('js-toggled');
-        this.toggleButton.setAttribute('aria-expanded', 'false');
-      } else {
-        this.toggleButton.setAttribute('aria-expanded', 'true');
-      }
-    });
+  // Toggle the .js-toggled class and the aria-expanded value each time the button is clicked.
+  handleToggle() {
+    if (this.toggleButton.getAttribute('aria-expanded') === 'true') {
+      ResponsiveNav.siteNavigation.classList.toggle('js-toggled');
+      this.toggleButton.setAttribute('aria-expanded', 'false');
+    } else {
+      this.toggleButton.setAttribute('aria-expanded', 'true');
+    }
+  }
+
+  handleClickOffMenu (event) {
+    const isClickInside = ResponsiveNav.siteNavigation.contains(event.target);
+    if (!isClickInside) {
+      ResponsiveNav.siteNavigation.classList.remove('js-toggled');
+      this.toggleButton.setAttribute('aria-expanded', 'false');
+    }
+  }
+
+  initEventHandlers() {
+    this.toggleButton.addEventListener('click', this.handleToggle );
+      
     // Remove the .js-toggled class and set aria-expanded to false when the user clicks outside the navigation.
-    document.addEventListener('click', function (event) {
-      const isClickInside = siteNavigation.contains(event.target);
-      if (!isClickInside) {
-        siteNavigation.classList.remove('js-toggled');
-        this.toggleButton.setAttribute('aria-expanded', 'false');
-      }
-    });
+    document.addEventListener('click', this.handleClickOffMenu);
   }
 }
