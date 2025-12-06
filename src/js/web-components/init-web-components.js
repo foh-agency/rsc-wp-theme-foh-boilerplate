02b72e701180616144705a4a0f62dae59c4ad17a
Eva Larumbe <evalarumbe@gmail.com>
2025-12-06 12:33:40 +0100

    Revert "Refactor to enable mobile menu (progressive enhancement, drawer, etc)…" (#6)
    This reverts commit 86668b4b1c1324f6fb154c7c4cefa2c494df8060, to add better docs

--- src/js/web-components/init-web-components.js
+++
@@ -1,7 +0,0 @@
-import ResponsiveNav from './foh-responsive-nav';
-import MainNavContent from './foh-main-nav-content';
-
-export function initWebComponents() {
-  customElements.define('foh-responsive-nav', ResponsiveNav);
-  customElements.define('foh-main-nav-content', MainNavContent);
-}
