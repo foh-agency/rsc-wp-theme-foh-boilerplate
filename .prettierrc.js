44cb222f1b8d0eba608b340bfe1a3b3f65ca9594
Eva Larumbe <evalarumbe@gmail.com>
2026-02-12 20:26:44 +0100

    Codestyle (#3)
    * Run prettier
    
    * Update readme

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-	...require( '@wordpress/prettier-config' ),
+	...require('@wordpress/prettier-config'),
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,20 +4,20 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  plugins:[
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-    })
-  ],
+	mode: 'production',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'static',
+		}),
+	],
 });
