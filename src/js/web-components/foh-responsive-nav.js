de1c5824c12a52a35702356b0e24227652d98b55
Eva Larumbe <evalarumbe@gmail.com>
2026-02-12 20:37:36 +0100

    Refactor to enable mobile menu (progressive enhancement, drawer, etc) (#5)
    * WIP, errors: copy in the menu from lab (to be adapted)
    
    * Set vars for menu mixin
    
    * WIP: bring in lab styles without vars
    
    * Remove reference to mini-menu
    
    * Menu styling wip: hide toggle button on desktop
    
    * WIP No-JS nav: Make menu slide in from top when link is clicked
    
    * Left-align the menu
    
    * No-JS menu: Keep header visible on scroll
    
    * No-JS menu: Scroll past menu to page content
    
    * Wip: use same breakpoints as tailwind. quick draft
    
    * No-JS menu: Hide logo so open drawer doesn't cause overflow-x
    
    * No-JS: Create links for open and close
    
    * Remove unused styles from _menu.scss
    
    * Tidy styles for responsive header menu
    
    * Refactor as custom element
    
    * WIP: Refactor FohMainNavContent as Custom Element
    
    * WIP: Revert nested custom element to repair styles that wouldn't render
    
    * Menu drawer: Make whole bar clickable for each list item
    
    * Remove old scripts from before refactor
    
    * Fix custom component
    
    * Fix self, add comment
    
    * Revert debugging experiment, we don't need a static property
    
    * Replace links with toggleButton once JS loads
    
    * Enable toggleButton
    
    * Remove class that's no longer needed since we have container queries checking aria-expanded
    
    * Update comments
    
    * Update docs
    
    * Remove duplicate import
    
    * Reorganize / refactor JS
    
    * Add disclaimer for untested feature ported from Underscores
    
    * Hide toggleButton on large screens

---
+++ src/js/web-components/foh-responsive-nav.js
@@ -0,0 +1,74 @@
+/**
+ * Handles toggling the navigation menu for small screens.
+ * Some of this functionality was included with Underscores in navigation.js
+ */
+export default class ResponsiveNav extends HTMLElement {
+  constructor() {
+    super();
+    this.siteNavigation = document.querySelector('#site-navigation');
+    // Return early if the navigation don't exist.
+    if (!this.siteNavigation) {
+      return;
+    }
+    // Return early if the button doesn't exist.
+    this.toggleButton = this.siteNavigation.querySelector('.js-menu-toggle-button');
+    if ('undefined' === typeof this.toggleButton) {
+      return;
+    }
+    // Hide menu toggle button if menu is empty and return early.
+    this.menu = this.siteNavigation.getElementsByTagName('ul')[0];
+    if ('undefined' === typeof this.menu) {
+      this.toggleButton.style.display = 'none';
+      return;
+    }
+
+    // Bind event handlers
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClickOffMenu = this.handleClickOffMenu.bind(this);
+  }
+
+  // Show the JS-enabled toggleButton, instead of the no-JS open/close links
+  initMenuToggle() {
+    const openLink = this.querySelector('.js-menu-link--open');
+    const closeLink = this.querySelector('.js-menu-link--close');
+
+    if (openLink) { openLink.classList.add('hidden'); }
+    if (closeLink) { closeLink.classList.add('hidden'); }
+    // Allow the toggleButton to appear...
+    this.toggleButton.classList.remove('hidden');
+    // ...except on larger screens, where we don't use the drawer
+    this.toggleButton.classList.add('md:hidden');
+  }
+
+  // Toggle aria-expanded value each time the button is clicked.
+  handleToggle() {
+    if (this.toggleButton.getAttribute('aria-expanded') === 'true') {
+      this.toggleButton.setAttribute('aria-expanded', 'false');
+    } else {
+      this.toggleButton.setAttribute('aria-expanded', 'true');
+    }
+  }
+
+  handleClickOffMenu (event) {
+    const isClickInside = this.siteNavigation.contains(event.target);
+    if (!isClickInside) {
+      this.toggleButton.setAttribute('aria-expanded', 'false');
+    }
+  }
+
+  // TODO: check these things are running
+  connectedCallback() {
+    this.initMenuToggle();
+
+    this.toggleButton.addEventListener('click', this.handleToggle );
+      
+    // Close the menu when the user clicks outside the navigation.
+    document.addEventListener('click', this.handleClickOffMenu);
+  }
+  
+  disconnectedCallback() {
+    // Remove event listeners
+    this.toggleButton.removeEventListener('click', this.handleToggle );
+    document.removeEventListener('click', this.handleClickOffMenu);
+  }
+}
